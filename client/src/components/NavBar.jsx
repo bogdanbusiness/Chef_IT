74068284aa3b759a80877b2a027a98eae5198a0a
bogdanbusiness <gheorghebogdan000@gmail.com>
2024-11-24 18:54:56 +0200

    Implemented desktop navigation bar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,36 +1,56 @@
 import './styles/navBar.css'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import MenuButton from './menuButton';
 
+// CSS variable from .env
 document.documentElement.style.setProperty('--nav-mobileWidth', import.meta.env.VITE_navBarWidth + "vw");
 
 function NavBar() {
+  // Use state to know when the menu image has been clicked
   const [clicked, setClicked] = useState(false);
 
   function handleOnClick() {
     if (clicked == false)
-      setClicked(true);
+       setClicked(true);
   }
-
+  
   function handleOffClick() {
     if (clicked == true)
       setClicked(false);
   }
 
+  // Changes the state of the site to true on resize
+  useEffect (() => {
+    function handleResize() {
+      setClicked(true);
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  // Moves the element to the right on click
   function moveRight () {
-    return clicked == false ? - import.meta.env.VITE_navBarWidth : 0;
+    // Change the state to true when on desktop
+    if (window.innerWidth > import.meta.env.VITE_mobileTreshold) {
+      return 0;
+    }
+
+    // Return by how much the element needs to be moved
+    return clicked == false ? - import.meta.env.VITE_navBarWidth - 10 : 0; // -10 for the box shadow
   }
 
+  // Blurs the entire page on click
   function blurBackground () {
-    return clicked == false ? `blur(0)` : `blur(${import.meta.env.VITE_navBarBackgroundBlur}px)`
+    return clicked == false ? -100 : 0
   }
 
   return (
-    <>
+    <div className='navBarContainer'>
       <MenuButton click={clicked} handleClick={handleOnClick}/>
-      <span className='divBgBlur' onClick={handleOffClick} style={{ backdropFilter: `blur(${blurBackground}px)` }} />
+      <span className='divBgBlur' onClick={handleOffClick} style={{ transform: `translateX(${blurBackground()}vw)` }} />
       <span className='navBar' style={{ transform: `translateX(${moveRight()}vw)` }} /> 
-    </>
+    </div>
   );
 }
 
