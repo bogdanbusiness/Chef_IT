1036e0d94eb1447f4cbc5c2e36f3a58851919c5d
bogdanbusiness <gheorghebogdan000@gmail.com>
2024-12-08 13:15:53 +0200

    created login webpage

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,7 +1,11 @@
 import './styles/loginForm.css';
+import emailIcon from '../assets/email_icon.png';
+import lockIcon from '../assets/lock_icon.png';
 import { useState } from 'react';
 
 document.documentElement.style.setProperty('--loginForm-primaryColor', "#" + import.meta.env.VITE_primaryColor);
+document.documentElement.style.setProperty('--loginForm-fontName', import.meta.env.VITE_fontName);
+document.documentElement.style.setProperty('--loginForm-fontSize', import.meta.env.VITE_fontSize);
 
 function LoginForm() {
   const [email, setEmail] = useState('');
@@ -37,25 +41,37 @@ function LoginForm() {
 
   return (
     <div className='loginBody'>
-      <div className='loginForm'>
-        <form onSubmit={handleSubmit}>
-          <input type="text" 
-            id="email" 
-            name="email" 
-            placeholder='Email' 
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input type="text"
+      <form className='loginForm' onSubmit={handleSubmit}>
+        <div className='loginTitle'> Loghează-te,<br/> chiorăie mațele! </div>
+        <div className='loginInputsContainer'>
+          <div className='loginInputContainer'>
+            <img src={emailIcon} className='loginIcons' />
+            <input type="text" className='loginInput'
+              id="email" 
+              name="email" 
+              placeholder='Email' 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className='loginInputContainer'>
+            <img src={lockIcon} className='loginIcons' />
+            <input type="text" className='loginInput'
             id="password"
             name="password"
             placeholder='Password'
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-          />
+            />
+          </div>
+
+        </div>
+
+        <div className='loginSubmitForgotContainer'>
           <input type="submit" value="Log In" className='submitButton' />
-        </form>
-      </div>
+          <a className="loginForgotPasswordLink" href="/newpassword"> Forgot Password </a>
+        </div>
+      </form>
     </div>
   );
 };
